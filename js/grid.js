5d3e5532a1661f788a7a7656cdb0a836b738c142
Brandon Mills <mills.brandont@gmail.com>
2014-04-04 18:46:53 -0400

    Add grid drawing library

---
+++ js/grid.js
@@ -0,0 +1,60 @@
+"use strict";
+var __moduleName = "grid";
+var Grid = function($el) {
+  var SIZE = 20;
+  var WEIGHT = 2;
+  var ctx = $el.getContext('2d');
+  function pixelHeight(val) {
+    if (arguments.length > 0) {
+      ctx.canvas.height = val;
+    }
+    return ctx.canvas.height;
+  }
+  function pixelWidth(val) {
+    if (arguments.length > 0) {
+      ctx.canvas.width = val;
+    }
+    return ctx.canvas.width;
+  }
+  function gridHeight(squares) {
+    if (arguments.length > 0) {
+      pixelHeight(squares * SIZE + (squares + 1) * WEIGHT);
+    }
+    return (pixelHeight() - WEIGHT) / (SIZE + WEIGHT);
+  }
+  function gridWidth(squares) {
+    if (arguments.length > 0) {
+      pixelWidth(squares * SIZE + (squares + 1) * WEIGHT);
+    }
+    return (pixelWidth() - WEIGHT) / (SIZE + WEIGHT);
+  }
+  function toPixels(coord) {
+    return (WEIGHT / 2) + coord * (WEIGHT + SIZE);
+  }
+  function clear() {
+    ctx.clearRect(0, 0, pixelWidth(), pixelHeight());
+  }
+  function line(x1, y1, x2, y2) {
+    var _lineCap = ctx.lineCap;
+    var _lineWidth = ctx.lineWidth;
+    var _strokeStyle = ctx.strokeStyle;
+    ctx.lineCap = 'round';
+    ctx.lineWidth = WEIGHT;
+    ctx.strokeStyle = 'black';
+    ctx.beginPath();
+    ctx.moveTo(toPixels(x1), toPixels(y1));
+    ctx.lineTo(toPixels(x2), toPixels(y2));
+    ctx.stroke();
+    ctx.lineCap = _lineCap;
+    ctx.lineWidth = _lineWidth;
+    ctx.strokeStyle = _strokeStyle;
+  }
+  return {
+    height: gridHeight,
+    width: gridWidth,
+    pixelHeight: pixelHeight,
+    pixelWidth: pixelWidth,
+    clear: clear,
+    line: line
+  };
+};
