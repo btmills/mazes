05d21d30733f9179e8bb7f07a3dd68414760f496
Brandon Mills <mills.brandont@gmail.com>
2014-11-06 00:08:53 -0500

    Minify generated js

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp       = require('gulp'),
     plumber    = require('gulp-plumber'),
     sourcemaps = require('gulp-sourcemaps'),
     to5        = require('gulp-6to5'),
-    concat     = require('gulp-concat');
+    concat     = require('gulp-concat'),
+    uglify     = require('gulp-uglify');
 
 var paths = {
 	src: ['src/**/!(app)*.js', 'src/app.js'],
@@ -14,7 +15,8 @@ gulp.task('build', function () {
 		.pipe(plumber())
 		.pipe(sourcemaps.init())
 		.pipe(to5())
-		.pipe(concat('index.js'))
+		.pipe(concat('app.min.js'))
+		.pipe(uglify())
 		.pipe(sourcemaps.write('.', {
 			includeContent: false,
 			sourceRoot: '..'
