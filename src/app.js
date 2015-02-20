81dfc70828c032e494c01d31a283b5219069f7b8
Brandon Mills <mills.brandont@gmail.com>
2015-02-20 05:16:11 -0500

    Rewrite App in ES6

--- src/app.js
+++ src/app.js
@@ -1,51 +1,71 @@
-(function () {
+import Maze from './maze';
+import random from './random';
 
-var Maze = require('./maze');
-var random = require('./random');
+class App {
+	get width() { return +this.$width.value; }
+	set width(value) { this.$width.value = value; }
 
-var maze;
+	get height() { return +this.$height.value; }
+	set height(value) { this.$height.value = value; }
 
-function generate() {
-	var width = +document.getElementById('width').value;
-	var height = +document.getElementById('height').value;
+	constructor() {
+		this.$width = document.querySelector('#width');
+		this.$height = document.querySelector('#height');
+		this.$permalink = document.querySelector('#permalink');
+		this.$canvas = document.querySelector('#canvas');
 
-	var hash = '#' + width + 'x' + height + 's' + random.seed;
-	var href = window.location.href.split('#')[0] + hash;
-	document.getElementById('permalink').setAttribute('href', href);
+		document.querySelector('#generate')
+			.addEventListener('click', (event) => {
+				event.preventDefault();
+				this.clearPermalink();
+				this.generate();
+			});
 
-	maze = new Maze(width, height);
-	maze.render(document.getElementById('canvas'));
+		document.body.addEventListener('keyup', (event) => {
+			let key = event.key || event.charCode || event.keyCode;
+			let direction = ['west', 'north', 'east', 'south'][key - 37];
+			if (!direction) return;
+
+			event.preventDefault();
+			this.maze.move(direction);
+		});
+
+		this.$canvas.addEventListener('mousemove', (event) => {
+			this.maze.hover(event.layerX, event.layerY);
+		});
+
+		// Attempt to read maze parameters from URL hash
+		let [, width, height, seed] = /#(\d+)x(\d+)(?:s(\d+.\d+))?|.*/
+			.exec(window.location.hash);
+
+		if (width && height) {
+			this.width = width;
+			this.height = height;
+		}
+
+		if (seed) {
+			random.seed = parseFloat(seed);
+		}
+
+		this.generate();
+	}
+
+	generate() {
+		this.setPermalink();
+
+		this.maze = new Maze(this.width, this.height);
+		this.maze.render(this.$canvas);
+	}
+
+	setPermalink() {
+		let hash = `#${this.width}x${this.height}s${random.seed}`;
+		let href = window.location.href.split('#')[0] + hash;
+		this.$permalink.setAttribute('href', href);
+	}
+
+	clearPermalink() {
+		window.location.href = window.location.href.split('#')[0];
+	}
 }
 
-document.getElementById('generate')
-	.addEventListener('click', function (event) {
-		event.preventDefault();
-		generate();
-	});
-document.body.addEventListener('keyup', function (event) {
-	var key = event.key || event.charCode || event.keyCode;
-	var direction = ['west', 'north', 'east', 'south'][key - 37];
-	if (!direction) return;
-
-	event.preventDefault();
-	maze.move(direction);
-});
-
-var [, width, height, seed] = /#(\d+)x(\d+)(?:s(\d+.\d+))?|.*/
-	.exec(window.location.hash);
-if (width && height) {
-	document.getElementById('width').value = width;
-	document.getElementById('height').value = height;
-}
-if (seed) {
-	random.seed = parseFloat(seed);
-}
-
-var canvas = document.getElementById('canvas');
-canvas.addEventListener('mousemove', function (event) {
-	maze.hover(event.layerX, event.layerY);
-});
-
-generate();
-
-})();
+new App();
